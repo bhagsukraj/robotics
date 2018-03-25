54dac1b620da6a44fc4d1084e7625868d74dda62
Ravi kiran <bhagsukraj@gmail.com>
2018-03-25 21:53:16 +0530

    update

---
+++ catkin_ws/build/pkg1/CMakeFiles/pkg1_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pkg1_node.dir/src/helloworld.cpp.o"
+  "/home/ravi/robotics/catkin_ws/devel/.private/pkg1/lib/pkg1/pkg1_node.pdb"
+  "/home/ravi/robotics/catkin_ws/devel/.private/pkg1/lib/pkg1/pkg1_node"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pkg1_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
